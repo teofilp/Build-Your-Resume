27e7d214bcef4f0343d1d97e917f200bd00cf9a8
tudyfilp <tudorf90@hotmail.com>
2019-03-26 22:01:43 +0200

    started resume builder part

--- src/components/ResumeBuilder.vue
+++ src/components/ResumeBuilder.vue
@@ -1,23 +1,60 @@
 <template>
-    <div id="builder_wrapper">
-        
+  <div id="builder_wrapper" class="container">
+    <div class="row mt-5">
+      <div id="resume_title_wrapper" class="col-md-4 offset-md-4">
+        <input type="text" id="resume_title" value="Untitled" placeholder="Untitled">
+        <div id="resume_title_wrapper_after"></div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    created(){
-        console.log("from resume builder");
-    }
-}
+  created() {
+    console.log("from resume builder");
+  }
+};
 </script>
 <style>
-    #builder_wrapper {
-        position: absolute;
-        left: 0;
-        top: 0;
-        height: 100%;
-        width: 50%;
-        overflow-y: auto;
-        background: red;
-    }
+#builder_wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  overflow-y: auto;
+  background: white;
+}
+
+#resume_title {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  text-align: center;
+  border: none;
+  font-size: 1.5em;
+  color: black;
+}
+::-webkit-input-placeholder {
+  color: black;
+}
+
+#resume_title_wrapper_after {
+  position: absolute;
+  content: "";
+  top: 100%;
+  left: 50%;
+  width: 0;
+  height: 2px;
+  background: black;
+  transition: left 0.2s, width 0.2s;
+}
+#resume_title:focus {
+  outline: none;
+}
+
+#resume_title:focus ~ #resume_title_wrapper_after {
+  left: 0;
+  width: 100%;
+}
 </style>
--- src/components/ResumePreview.vue
+++ src/components/ResumePreview.vue
@@ -1,121 +1,142 @@
 <template>
-    <div id="preview_wrapper">
-        <div id="resume_preview">
-            <div class="resume_overlay">
-                <i class="fas fa-eye"></i>
-            </div>
-        </div>
-        <ul class="options">
-            <li><div><h2><i class="fas fa-share"></i> Share</h2></div></li>
-            <li><button class="download">Download</button></li>
-            <li><div><h2><i class="fas fa-eye"></i>Preview</h2></div></li>
-            
-        </ul>
+  <div id="preview_wrapper">
+    <div id="resume_preview">
+      <div class="resume_overlay">
+        <i class="fas fa-eye"></i>
+      </div>
     </div>
+    <ul class="options">
+      <li>
+        <div>
+          <h2>
+            <i class="fas fa-share"></i> Share
+          </h2>
+        </div>
+      </li>
+      <li>
+        <button class="download">Download</button>
+      </li>
+      <li>
+        <div>
+          <h2>
+            <i class="fas fa-eye"></i>Preview
+          </h2>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
-    mounted(){
-        let resume_preview =  document.querySelector("#resume_preview"); 
-        resume_preview.style.height = resume_preview.offsetWidth * 1.41 + "px";
-    }
-}
+  mounted() {
+    let resume_preview = document.querySelector("#resume_preview");
+    let resume_options = document.querySelector(".options");
+    resume_preview.style.height = resume_preview.offsetWidth * 1.41 + "px";
+    resume_options.style.top =
+      resume_preview.offsetHeight / 2 + resume_preview.offsetTop + 30 + "px";
+  }
+};
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css?family=Open+Sans');
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
 * {
-    font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
-    #preview_wrapper {
-        position: absolute;
-        left: 50%;
-        top: 0;
-        height: 100%;
-        width: 50%;
-        background: #aaa;
-        overflow-y: auto;
-    }
+#preview_wrapper {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  height: 100%;
+  width: 50%;
+  background: #aaa;
+  overflow-y: auto;
+}
 
-    #resume_preview {
-        position: relative;
-        width: 60%;
-        left: 50%;
-        top: 45%;
-        transform: translate(-50%, -50%);
-        background: white;
-        box-shadow: 2px 2px 15px #222;
-    }
-    .resume_overlay {
-        position: relative;
-        width: 100%;
-        height: 100%;
-    }
-    .resume_overlay:hover {
-        cursor: pointer;
-    }
-    .resume_overlay:hover i {
-        font-size: 40px;
-        padding: 15px;
-    }
-    .resume_overlay i {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        font-size: 0px;
-        padding: 0px;
-        background: #2d6fd8;
-        color: white;
-        border-radius: 50%;
-        transition: font-size .1s, padding .1s;
-    }
-    .options {
-        display: table;
-        margin: 0;
-        padding: 0;
-        width: 60%;
-        height: 60px;
-        margin-top: 70px;
-        margin-left: 20%;
-    }
+#resume_preview {
+  position: relative;
+  width: 55%;
+  left: 50%;
+  top: 45%;
+  transform: translate(-50%, -50%);
+  background: white;
+  box-shadow: 2px 2px 15px #222;
+}
 
-    .options li {
-        display: table-cell;
-        text-align: center;
-        height: 100%;
-    }
+.resume_overlay {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 
-    .options li div h2{
-        color: white;
-        font-weight: 300;
-        font-size: 1em;
-        border-radius: 12px;
-        padding: 8px 8px;
-        transition: background-color .3s;
-    }
+.resume_overlay:hover {
+  cursor: pointer;
+}
 
-    .options li:hover div h2 {
-        background: #999;
-        cursor: pointer;
-    }
+.resume_overlay:hover i {
+  font-size: 32px;
+  padding: 12px;
+}
 
-    button.download {
-        background: #2d6fd8;
-        font-size: 1.2em;
-        border: none;
-        padding: 15px 30px;
-        border-radius: 8px;
-        color: white;
-        transition: background-color .3s;
-    }
-    i {
-        padding-right: 5px;
-    }
+.resume_overlay i {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 0px;
+  padding: 0px;
+  background: #2d6fd8;
+  color: white;
+  border-radius: 50%;
+  transition: font-size 0.1s, padding 0.1s;
+}
 
-    button.download:hover {
-        cursor: pointer;
-        background: rgb(25, 91, 196);
-    }
+.options {
+  position: absolute;
+  display: table;
+  padding: 0;
+  width: 60%;
+  height: 60px;
+  left: 20%;
+}
+
+.options li {
+  display: table-cell;
+  text-align: center;
+  height: 100%;
+}
+
+.options li div h2 {
+  color: white;
+  font-weight: 300;
+  font-size: 1em;
+  border-radius: 12px;
+  padding: 8px 8px;
+  transition: background-color 0.3s;
+}
+
+.options li:hover div h2 {
+  background: #999;
+  cursor: pointer;
+}
+
+button.download {
+  background: #2d6fd8;
+  font-size: 1.2em;
+  border: none;
+  padding: 15px 30px;
+  border-radius: 8px;
+  color: white;
+  transition: background-color 0.3s;
+}
+
+i {
+  padding-right: 5px;
+}
+
+button.download:hover {
+  cursor: pointer;
+  background: rgb(25, 91, 196);
+}
 </style>
 
 
