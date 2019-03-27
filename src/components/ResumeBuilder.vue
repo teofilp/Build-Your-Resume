588d7800f97e27eb2529737b7e433dee644017f9
tudyfilp <tudorf90@hotmail.com>
2019-03-27 15:41:35 +0200

    new resume preview builder feature added

--- src/components/ResumeBuilder.vue
+++ src/components/ResumeBuilder.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="builder_wrapper" class="container">
-    <div class="row mt-5">
-      <div id="resume_title_wrapper" class="col-md-4 offset-md-4">
-        <input type="text" id="resume_title" value="Untitled" placeholder="Untitled">
+    <div class="row mt-5 resume_title_section">
+      <div id="resume_title_wrapper" class="col-md-6 offset-md-3">
+        <input type="text" class="col-md-4 offset-md-4" id="resume_title"
+         placeholder="Untitled" @click="selectTitle" :value="getTitle">
+        <i class="fas fa-edit col-md-1 rename_title" @click="selectTitle"></i>
         <div id="resume_title_wrapper_after"></div>
+      </div>
+    </div>
+    <div class="row resume_completeness_section px-5 mt-5">
+      <div class="row col-md-12">
+        <div class="col-md-6">
+          <span class="text-danger mr-2">10%</span> Profile completeness
+        </div>
+        <div class="col-md-6">
+          <span style="float: right"><span class="text-success mr-2">+10%</span> Add Profile Summary</span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex';
 export default {
   created() {
     console.log("from resume builder");
+  },
+  methods: {
+    selectTitle(){
+      document.getElementById("resume_title").select();
+    }
+  },
+  computed: {
+    ...mapGetters(['getTitle'])
   }
 };
 </script>
@@ -32,10 +53,20 @@ export default {
   padding: 0;
   text-align: center;
   border: none;
-  font-size: 1.5em;
+  font-size: 1.7em;
   color: black;
 }
 ::-webkit-input-placeholder {
+  color: black;
+}
+
+.rename_title {
+  font-size: 1.3em;
+  color: #ccc;
+  transition: color .3s;
+}
+.rename_title:hover {
+  cursor: pointer;
   color: black;
 }
 
@@ -45,7 +76,7 @@ export default {
   top: 100%;
   left: 50%;
   width: 0;
-  height: 2px;
+  height: 3px;
   background: black;
   transition: left 0.2s, width 0.2s;
 }
@@ -54,7 +85,11 @@ export default {
 }
 
 #resume_title:focus ~ #resume_title_wrapper_after {
-  left: 0;
+  left: 33%;
+  width: 33%;
+}
+
+.resume_completeness_section {
   width: 100%;
 }
 </style>
