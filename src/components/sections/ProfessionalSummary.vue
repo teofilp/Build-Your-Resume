587a641c3d65e6b864feef64ce1657d69e2222e6
tudyfilp <tudorf90@hotmail.com>
2019-03-28 12:41:17 +0200

    added icons for stylers

--- src/components/sections/ProfessionalSummary.vue
+++ src/components/sections/ProfessionalSummary.vue
@@ -10,12 +10,40 @@
         class="col-md-11"
         rows="5"
       ></textarea>
-      <div class="col-md-11 stylers"></div>
+      <div class="col-md-11 stylers">
+        <ul>
+          <li @click="execCommand('bold')"><i class="fas fa-bold"
+          :class="{active : commands.bold}"></i></li>
+          <li @click="execCommand('italic')"
+          ><i class="fas fa-italic"
+          :class="{active : commands.italic}"></i></li>
+          <li @click="execCommand('underline')"><i class="fas fa-underline"
+          :class="{active : commands.underline}"></i></li>
+          <li><i class="fas fa-backspace inactive"></i></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      commands: {
+        italic: false,
+        bold: false,
+        underline: false
+       }
+    }
+  },
+  methods: {
+    execCommand(command){
+      this.commands[command] = !this.commands[command];
+      console.log(this.commands[command]);
+      document.execCommand(command);
+    }
+  }
+};
 </script>
 <style scoped>
 .wrapper {
@@ -49,6 +77,41 @@ textarea::-webkit-input-placeholder {
 div.stylers {
   background: #eee;
   height: 40px;
+}
+
+.stylers ul {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+  list-style: none;
+}
+
+.stylers ul li {
+  display: inline-block;
+  margin-left: 12px;
+}
+
+.stylers ul li i {
+  transition: color .3s;
+}
+
+.stylers ul li i {
+  font-size: .8em;
+}
+
+.stylers ul li:hover i {
+  color: #32abdb;
+  cursor: pointer;
+}
+
+.stylers ul li i.active {
+  color: #32abdb;
+}
+
+.stylers ul li i.inactive {
+  color: #bbb;
+  cursor: not-allowed;
 }
 </style>
 
