3b4a3971d0e5eda02a4a9e207b2828b97e1b04fc
tudyfilp <tudorf90@hotmail.com>
2019-04-30 22:08:57 +0300

    make it better

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 import ResumeApp from './components/ResumeApp.vue';
 import Home from './components/Home.vue';
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [{
     path: '/app', component: ResumeApp
